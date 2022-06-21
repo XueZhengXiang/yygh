412c6713adaf44afb4f0550528191a84398d0846
xiangGe <2235915272@qq.com>
2022-06-21 22:32:45 +0800

    commit gatway

---
+++ service_gateway/src/main/java/com/atguigu/yygh/gateway/ServerGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.yygh.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lijian
+ * @create 2021-04-29 9:13
+ */
+@SpringBootApplication
+public class ServerGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerGatewayApplication.class, args);
+    }
+}
---
+++ service_gateway/src/main/java/com/atguigu/yygh/gateway/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package com.atguigu.yygh.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author lijian
+ * @create 2021-04-29 9:28
+ */
+
+//跨域处理
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+}
+
+}
