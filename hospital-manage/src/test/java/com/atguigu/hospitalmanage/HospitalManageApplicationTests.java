d91acb539a2afb4fb3655d64cf9aaa68a4819d66
xiangGe <2235915272@qq.com>
2022-06-21 22:29:53 +0800

    commit hosptial-mange

---
+++ hospital-manage/src/test/java/com/atguigu/hospitalmanage/HospitalManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.hospitalmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
